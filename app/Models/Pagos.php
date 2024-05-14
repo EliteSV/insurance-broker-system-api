3053de552c879081f0a6ef0111ea76580faa6211
Josue Garcia <josue@sapphiretechglobal.com>
2024-05-14 02:03:45 -0600

    Feat: introducing pagos

---
+++ app/Models/Pagos.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pagos extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['vigencia_poliza_id', 'cantidad', 'fecha_vencimiento', 'fecha_pagado', 'comprobante', 'estado'];
+
+    public function vigencia()
+    {
+        return $this->belongsTo(VigenciaPolizas::class, 'vigencia_poliza_id');
+    }
+}
