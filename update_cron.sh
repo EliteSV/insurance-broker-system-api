8c9a4fd0760112e09d549feb8bb39a73f3543229
Josue Garcia <josue@sapphiretechglobal.com>
2024-05-17 23:51:00 -0600

    feat: Adding .ebextensions

--- update_cron.sh
+++ update_cron.sh
@@ -6,4 +6,4 @@ CRON_JOB="* * * * * cd $CURRENT_PATH && php artisan schedule:run >> /dev/null 2>
 
 (crontab -l | grep -F "$CRON_JOB") || (crontab -l ; echo "$CRON_JOB") | crontab -
 
-echo "Cron job added to run every minute: $CRON_JOB"
+echo "Cron job added to run daily: $CRON_JOB"
