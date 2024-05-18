640fda8333e6a77ca8ca835712c92054a3d19e82
Josue Garcia <josue@sapphiretechglobal.com>
2024-05-18 02:28:11 -0600

    feat: Adding pagos to renovacion

--- app/Http/Controllers/RenovacionController.php
+++ app/Http/Controllers/RenovacionController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Poliza;
 use App\Models\VigenciaPolizas;
+use App\Services\PolizaService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class RenovacionController extends Controller
 {
+    protected $polizaService;
+
+    public function __construct(PolizaService $polizaService)
+    {
+        $this->polizaService = $polizaService;
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -16,14 +24,20 @@ class RenovacionController extends Controller
     {
         try {
             $request->validate([
-                'fecha_inicio' => 'required|max:255',
-                'fecha_vencimiento' => 'required|max:255',
+                'fecha_inicio' => 'required|date',
+                'fecha_vencimiento' => 'required|date',
                 'poliza_id' => 'required|exists:polizas,id'
             ]);
 
             $vigenciaData = $request->all();
+            $vigenciaPoliza = VigenciaPolizas::create($vigenciaData);
 
-            return VigenciaPolizas::create($vigenciaData);
+            $poliza = Poliza::find($vigenciaData['poliza_id']);
+            $this->polizaService->createPagos($vigenciaPoliza, $poliza->monto, $request->fecha_inicio, $poliza->cuotas);
+
+            $vigenciaPoliza->load(['pagos']);
+
+            return response()->json($vigenciaPoliza, 201);
         } catch (\Exception $e) {
             Log::error('Failed to store renovacion: ' . $e->getMessage());
             return response()->json(['error' => $e->getMessage()], 422);
---
+++ app/Services/PolizaService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Pagos;
+use App\Models\VigenciaPolizas;
+use Carbon\Carbon;
+
+class PolizaService
+{
+    public function createPagos(VigenciaPolizas $vigenciaPoliza, $monto, $fechaInicio, $cuotas)
+    {
+        $parsedDate = Carbon::parse($fechaInicio);
+
+        $intervalLoopMap = [
+            1 => [12, 0],
+            3 => [3, 2],
+            6 => [6, 1],
+            12 => [1, 11]
+        ];
+
+        [$interval, $iterations] = $intervalLoopMap[$cuotas];
+
+        $cantidad = round($monto / ($iterations + 1), 2);
+
+        Pagos::create([
+            'vigencia_poliza_id' => $vigenciaPoliza->id,
+            'cantidad' => $cantidad,
+            'fecha_vencimiento' => $parsedDate->format('Y-m-d'),
+            'fecha_pagado' => null,
+            'comprobante' => null,
+            'estado' => 'Pendiente'
+        ]);
+
+        for ($i = 1; $i <= $iterations; $i++) {
+            $fechaVencimiento = $parsedDate->copy()->addMonths($interval * $i)->format('Y-m-d');
+
+            Pagos::create([
+                'vigencia_poliza_id' => $vigenciaPoliza->id,
+                'cantidad' => $cantidad,
+                'fecha_vencimiento' => $fechaVencimiento,
+                'fecha_pagado' => null,
+                'comprobante' => null,
+                'estado' => 'Pendiente'
+            ]);
+        }
+    }
+}
