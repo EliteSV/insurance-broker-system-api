a8f0cec12edbf4ea9e447471d2861b103f5e6d7a
Josue Garcia <josue@sapphiretechglobal.com>
2024-05-17 23:46:28 -0600

    fix: Updating command in console.php

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('pagos:update-status')->daily();
+Schedule::command('pagos:verificar-pagos')->daily();
